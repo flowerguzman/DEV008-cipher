91e7316e49337df44003627b1331452b314a34b2
flowerguzman <flowergzm050@gmail.com>
2023-05-14 22:48:38 -0600

    imagen2

--- src/cipher.js
+++ src/cipher.js
@@ -3,3 +3,60 @@ const cipher = {
 };
 
 export default cipher;
+
+const btnCifrar = document.querySelector ('#cifrar');
+const btnDescifrar = document.querySelector ('#descifrar');
+const key = document.querySelector ('#key');
+
+const listLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+let newText = '';
+
+btnCifrar.addEventListener ('click' , () => {
+  const textarea = document.querySelector ('#text');
+  const keyValue = Number (key.value);
+  for (let letter of textarea.value){
+    letter = letter.toUpperCase();
+    if (!listLetters.includes(letter)) {
+      continue
+    }
+
+    const indexLetter = listLetters.findIndex ((item) => item === letter);
+    let indexNewLetter = indexLetter + keyValue
+
+    if(indexNewLetter > 25) {
+      indexNewLetter -= 26;
+    }
+
+    newText += listLetters[indexNewLetter]
+    textarea.value = newText
+  }
+
+  newText = '';
+});
+
+btnDescifrar.addEventListener ('click' , () => {
+  const textarea = document.querySelector ('#text');
+  const keyValue = Number (key.value);
+  for (const letter of textarea.value){
+    if (!listLetters.includes(letter)) {
+      continue
+    }
+
+    const indexLetter = listLetters.findIndex ((item) => item === letter);
+    let indexNewLetter = indexLetter - keyValue
+
+    if(indexNewLetter < 0) {
+      indexNewLetter += 26;
+    }
+
+    newText += listLetters[indexNewLetter]
+    textarea.value = newText;
+  }
+
+  newText = '';
+});
+
+
+
+ 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
-var i=abc.indexOf(c.toUpperCase)
 
-let sitioWeb = "www.programacionfacil.org";
-console.log(sitioWeb.charCodeAt(4));
+
+
 
 
